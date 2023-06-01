c551d81d805022db9517d5a72083d7170efbfbf3
hckang17 <hckang17@naver.com>
2023-06-01 16:12:09 +0900

    4th commit

--- sugang/sugang/functions/method.py
+++ sugang/sugang/functions/method.py
@@ -4,6 +4,7 @@ import urllib.error
 import datetime
 from ..views import * 
 import speedtest_cli
+import numpy as np
 
 def get_accessurl_by_highest_id():
     highest_id = accessURL.objects.order_by('-id').values_list('id', flat=True).first()
@@ -15,6 +16,7 @@ def get_accessurl_by_highest_id():
     
 def calculate_time(targetURL):  #targetURL 은 URLtype
     response = urllib.request.urlopen(targetURL).headers['Date']
+    print(response)
     datetime_obj = datetime.datetime.strptime(response, "%a, %d %b %Y %H:%M:%S %Z")
     datetime_obj += datetime.timedelta(hours=9)
     # 원하는 형식으로 포맷팅
@@ -46,5 +48,36 @@ def checkSpeed():
     ping_speed = st.results.ping
     return up_speed, down_speed, ping_speed
     
+def get_speed_percentile(down_speed):
+    # 데이터베이스에 저장된 다운로드 속도 데이터를 가져옵니다.
+    speed_data = resultInfo.objects.all().values_list('downSpeed')
+    speed_data = [item[0] for item in speed_data]
+
+    # 속도 데이터가 존재하지 않으면 0을 반환합니다.
+    if not speed_data:
+        return 0
+
+    # 속도 데이터가 존재할 경우 각 속도에 대한 순위를 계산하고, down_speed의 순위를 구합니다.
+    rank = sum(down_speed > s for s in speed_data)
+    percentile = round(rank / len(speed_data) * 100, 2)
+    percentile = round(100 - percentile, 2)
     
-    
+    return percentile
+
+def checkUpLink():
+    st = speedtest_cli.Speedtest()
+    st.get_best_server()
+    up_speed = round(st.upload() / 1000000 , 2)
+    return up_speed
+
+def checkDownLink():
+    st = speedtest_cli.Speedtest()
+    st.get_best_server()
+    down_speed = round(st.download() / 1000000 , 2)
+    return down_speed
+
+def checkPing():
+    st = speedtest_cli.Speedtest()
+    st.get_best_server()
+    return round(st.results.ping, 1)
+
--- sugang/sugang/views.py
+++ sugang/sugang/views.py
@@ -2,10 +2,6 @@ import datetime
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import *
-import urllib.request
-import urllib.error
-import time
-import requests
 from .functions import method
 from django.http import JsonResponse
 
@@ -18,8 +14,7 @@ def index(request):
     return render(request, 'mainpage.html', context)
 
 def action(request):
-    context = {'current_servertime' : None, 'user_url': None, 'uplink' : None,
-               'downlink': None, 'pingspeed': None}
+    context = {}
     if request.method == 'POST':
         if request.POST.get('saveURL') == '':
             return redirect('sugang:main')
@@ -27,27 +22,10 @@ def action(request):
             method.save_URL(request)
             temp = method.show_server_time(method.get_accessurl_by_highest_id())
             context['current_servertime'], context['user_url'] = temp[0], temp[1]
-            temp = method.checkSpeed()
-            context['uplink'], context['downlink'], context['pingspeed'] = temp[0], temp[1], temp[2]
-            
             return render(request, 'mainpage.html', context)
     else:
         return redirect('sugang:main')
-        
-    return render()
 
-    
-'''
-    def show_server_time(request, saveURL):
-    try:
-        targetURL = saveURL.testURL
-        serverTime = method.calculate_time(targetURL)
-        context = {'current_servertime' : serverTime, 'user_url' : targetURL}
-        return render(request, 'mainpage.html', context)
-    except:
-        return HttpResponse("Could not retrieve server time.")
-'''
-    
 def reload_serverclock(request):
     target_url = method.get_accessurl_by_highest_id()
     server_time = method.calculate_time(target_url.testURL)
@@ -57,3 +35,26 @@ def reload_defaultclock(request):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y년 %m월 %d일 %H시 %M분 %S초")
     return JsonResponse({'current_servertime':formatted_time})
+
+def TestUpLink(request):
+    upSpeed = method.checkUpLink()
+    response = {'upSpeed':upSpeed}
+    return JsonResponse(response)
+
+def TestDownLink(request):
+    downSpeed = method.checkDownLink()
+    result = resultInfo.objects.create(
+        upSpeed = 0,
+        downSpeed= downSpeed,
+        pingSpeed= 0)
+    down_percentile = method.get_speed_percentile(downSpeed)
+    result.save()
+    response = {'downSpeed':downSpeed, 'speed_ranking':down_percentile}    
+    return JsonResponse(response)
+
+def TestPing(request):
+    pingSpeed = method.checkPing()
+    response = {'pingSpeed':pingSpeed}
+    return JsonResponse(response)
+
+
